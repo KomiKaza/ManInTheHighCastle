53d7e98c0604adeba0b15acafa572a21ff02d00b
shahafvg <shahafvg@gmail.com>
2016-12-31 15:02:33 +0200

    ver 1

---
+++ Assets/Scripts/LoadScreen.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LoadScreen : MonoBehaviour {
+	public Text loadText;
+
+	// Use this for initialization
+	void Start () {
+		
+		InvokeRepeating ("disapear", 0, 1.7f);
+		InvokeRepeating ("appear", 0, 1.5f);
+		SceneManager.LoadScene ("MainGame");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void disapear()
+	{
+		loadText.text = "";
+	}
+
+	void appear()
+	{
+		loadText.text = "Loading . . .";
+	}
+
+	void move()
+	{
+		ExecuteAfterTime (2f);
+		loadText.gameObject.SetActive (true);
+	}
+
+	IEnumerator ExecuteAfterTime(float time)
+	{
+		yield return new WaitForSeconds(time);
+
+		loadText.gameObject.SetActive (false);
+
+	}
+
+}
---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class UiManager : MonoBehaviour {
+
+	bool notgameover;
+	public Button [] buttons;
+	bool pause=false;
+	private int pointerID;
+	float volume;
+	float sound;
+	bool saved=false;
+	public Text Savedtext;
+	public Slider[] sliders;
+
+
+
+
+
+	// Use this for initialization
+
+
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene ("Loading");
+	}
+
+	public void Credits()
+	{
+		SceneManager.LoadScene ("Credits");
+	}
+
+
+
+	public void goOptions(){
+		SceneManager.LoadScene ("Options");
+	}
+
+	public bool getPaused()
+	{
+		return pause;
+	}
+
+
+
+
+	public void adjustVolume(Slider slider)
+	{
+		volume = slider.value;
+	//	Debug.Log (slider.value);
+	}
+
+	public void adjustSound(Slider slider)
+	{
+		sound = slider.value;
+	//	Debug.Log (slider.value);
+	}
+
+	public void saveOptions()
+	{
+		PlayerPrefs.SetFloat ("Volume", volume);
+		PlayerPrefs.SetFloat ("Sound", sound);
+	//	Debug.Log (PlayerPrefs.GetFloat ("Volume"));
+
+		Savedtext.text = "SAVED";
+		saved = true;
+	}
+
+	public void backToMain(){
+		SceneManager.LoadScene ("MenuScene");
+	}
+
+}
