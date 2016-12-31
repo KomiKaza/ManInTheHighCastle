c09bad123125f0c6323f57233b3ffff67225f5d8
shahafvg <shahafvg@gmail.com>
2016-12-31 15:30:53 +0200

    ver 0.1

---
+++ Assets/Scripts/MainPlay.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainPlay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.orientation = ScreenOrientation.LandscapeLeft;//or right for right landscape
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
