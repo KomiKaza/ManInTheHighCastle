9247dbb464e85f6696842596905a6fcffc9699d5
shahafvg <shahafvg@gmail.com>
2017-01-02 15:18:19 +0200

    ver 1.5
    added all states (Circles)

---
+++ Assets/Scripts/StateCode.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateCode : MonoBehaviour {
+
+	public string name;
+	public double republican;
+	public double democrate;
+	public double floaters;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Color c = (floaters > (republican) && floaters > democrate) ? Color.white : (republican > democrate ? Color.red : Color.blue);
+	
+			GetComponent<SpriteRenderer> ().color = c;
+
+
+	}
+}
